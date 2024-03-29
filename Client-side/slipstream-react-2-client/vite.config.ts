ccbccef92508839dca53f4071a0c434e87b3538c
Niall-J-Murray <niall_murray@outlook.com>
2024-03-29 14:03:24 +0000

    Fixed typescript build errors

--- Client-side/slipstream-react-2-client/vite.config.ts
+++ Client-side/slipstream-react-2-client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     define: {
-        global: {},
+        define: { _global: ({}), }
     },
     plugins: [UnoCSS(), react()],
     server: {
